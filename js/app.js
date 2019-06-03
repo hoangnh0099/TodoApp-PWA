001040c19444952ee394863dcf559d866d3ab68e
Nguyen Huy Hoang <hoangnh0099@gmail.com>
2019-06-03 23:47:08 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -4,25 +4,36 @@ if ("serviceWorker" in navigator) {
     .catch((err) => console.log("Service Worker not registered", err));
 }
 
+
+
 const todoList = document.getElementById("todoList");
 const addButton = document.getElementById("addButton");
 const inputText = document.getElementById("inputText");
+const dateElement = document.getElementById("date");
+
+const option = { 
+  weekday: "long",
+  month: "short",
+  day: "numeric",
+}
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", option);
+
+const render = (data) => {
+  const dataToHTML = data.map(data => {
+    return `<li class="deteleButton">${data.content}</li>`;
+  });
+  todoList.innerHTML = dataToHTML.join("");
+};
+
+render(data);
 
 addButton.addEventListener("click", (event) => {
   event.preventDefault();
-  
   data.unshift({
     id: data.length + 1,
     content: inputText.value
   })
   inputText.value = "";
-
   render(data);
 });
-
-const render = (data) => {
-  const dataToHTML = data.map(data => {
-    return `<li>${data.content}</li>`;
-  });
-  todoList.innerHTML = dataToHTML.join("");
-};
